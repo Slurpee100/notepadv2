8b0385ba6175d73967a8f68e1ea428bc2245144c
Slurpee100 <87210266+Slurpee100@users.noreply.github.com>
2021-09-09 21:42:41 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# notepadv2
-Cool QBCore notepad 
+Slurpees QBCore Notepad script
+
+Add this to your shared.lua 
+["notepad"]                       = {["name"] = "notepad",                       ["label"] = "Notepad",                 ["weight"] = 0,         ["type"] = "item",         ["image"] = "notepad1.png",             ["unique"] = false,     ["useable"] = true,     ["shouldClose"] = false,    ["combinable"] = nil,   ["description"] = "A Notepad."},
+
+Find a image you like
+
+Add the image to your inventory
+
+Put the script in your [standalone] directory
+
+And have fun.
+
+Thanks Coyote#4558
---
+++ nui/js/app.js
@@ -0,0 +1,62 @@
+var ownerdNotepad, notepadreader;
+document.onkeyup = function (data) {
+    if (data.which == 27) {
+        $.post('http://notepad/escape', JSON.stringify({}));
+        if (notepadreader == true) {
+            let x = document.getElementById("p1").value;
+            $.post('http://notepad/updating', JSON.stringify({ text: x}));
+            $("#main").fadeOut();
+            $("#main").css('display', 'none');
+            notepadreader = false;
+            document.getElementById("p1").value = "";
+        }else {
+            ownerdNotepad = document.getElementById("p1").value;
+            $("#main").fadeOut();
+            $("#main").css('display', 'none');
+        }
+    }
+};
+
+function dropNotepad() {
+    let x = document.getElementById("p1").value;
+    if (x !== ""){
+        $.post('http://notepad/dropping', JSON.stringify({ text: x}));
+        $("#main").fadeOut();
+        document.getElementById("p1").value = "";
+        $("#main").css('display', 'none');
+    }
+}
+
+window.addEventListener('message', function(e) {
+    switch(event.data.action) {
+        case 'openNotepad':
+            if (ownerdNotepad === undefined){
+                document.getElementById("p1").value = "";
+                $("#main").fadeIn();
+            }else {
+                $("textarea").removeAttr('disabled','disabled');
+                $("button").fadeIn();
+                document.getElementById("p1").value = ownerdNotepad;
+                $("#main").fadeIn();
+            }
+            break;
+        case 'openNotepadRead':
+            notepadreader = true;
+            $("textarea").attr('disabled','disabled');
+            $("button").hide();
+            $("#main").fadeIn();
+            document.getElementById("p1").value = event.data.TextRead;
+            break;
+        case 'closeNotepad':
+            $("#main").fadeOut();
+            $("#main").css('display', 'none');
+            break;
+        case 'cleanNotepad':
+            document.getElementById("p1").value = "";
+            break;
+    }
+});
+
+
+
+
